020b90f2f7b8815dea6f800a89df311582f32355
Janis Mezitis <janis.mezitis@tieto.com>
2013-03-14 00:01:19 +0200

    Added support for applying auto unlock based on bluetooth connection strength

--- detect_phone.sh
+++ detect_phone.sh
@@ -5,18 +5,24 @@
 
 BTHW="xx:xx:xx:xx:xx:xx"  # Enter your Phone Bluetooth hardware address
 LOCKPRG="kscreenlocker"		# Screen saver program
+MIN_SIGNAL_STRENGTH=-5	  # Enter min signal strength at which we will do automatic unlock 
 SLEEP=5
 
-ME=`whoami`
 while true
 do	# Run only if screen is locked
-	PID=`pgrep -u $ME $LOCKPRG | head -1`
+	PID=$(pgrep -u $USER $LOCKPRG | head -1)
 	if [ .$PID != . ]; then
 		# Ping the phone (needs sudo permissions
 		# chmod u+s /usr/bin/l2ping
-		l2ping -t 5 -c 1 $BTHW >/dev/null 2>/dev/null
-		RC=$?
-		if [ $RC = 0 ]; then
+		#l2ping -t 5 -c 1 $BTHW >/dev/null 2>/dev/null
+		if [ -z "$(hcitool con | grep $BTHW)" ]; then
+			# need sudo
+			sudo hcitool cc $BTHW # if we arent connected to device then do temporar conection
+		fi
+
+		STR=$(hcitool rssi $BTHW | sed 's/[^0-9-]*//') # get signal signal strength (less is worse)
+		echo $STR
+		if [ $STR -gt $MIN_SIGNAL_STRENGTH ]; then
 			# Phone present...
 			kill $PID
 			xset dpms force on
