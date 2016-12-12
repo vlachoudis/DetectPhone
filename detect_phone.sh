522017cfa3ce2e46738d4631a1eb4460d9d0e4f0
vlachoudis <vvlachoudis@gmail.com>
2016-12-12 14:41:23 +0100

    Finaly found a way to kill the locker in KDE5

--- detect_phone.sh
+++ detect_phone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # author: Vasilis.Vlachoudis@cern.ch
-# version 0.2
-# date: 6 Feb 2012
+# version 0.3
+# date: 12 Dec 2016
 
 BTHW="XX:XX:XX:XX:XX:XX"	# Enter your Phone Bluetooth hardware address
 DESKTOP="x11"			# Enter desktop/screen saver to check
@@ -25,7 +25,7 @@ xscreensaver_kill() {
 }
 
 # return kscreenlocker state
-kscreenlocker_state() {
+kscreenlocker4_state() {
 	local pid=$(pgrep -u $ME kscreenlocker | head -1)
 	if [ .$pid != . ]; then
 		LOCKED=1
@@ -35,8 +35,25 @@ kscreenlocker_state() {
 }
 
 # kill kscreenlocker
-kscreenlocker_kill() {
+kscreenlocker4_kill() {
 	qdbus  | grep kscreenlocker_greet | xargs -I {} qdbus {} /MainApplication quit
+#	pkill -u $ME kscreenlocker
+}
+
+# return kscreenlocker state
+kscreenlocker5_state() {
+	local active=$(qdbus org.freedesktop.ScreenSaver /ScreenSaver GetActive)
+	echo "Active= $active"
+	if [ $active = true ]; then
+		LOCKED=1
+	else
+		LOCKED=0
+	fi
+}
+
+# kill kscreenlocker
+kscreenlocker5_kill() {
+	loginctl unlock-session
 }
 
 # return gnome_screensaver state
@@ -61,27 +78,36 @@ case "$DESKTOP" in
 		STATE_CMD=gnome_screensaver_state
 		KILL_CMD=gnome_screensaver_kill
 		;;
-	"kde" | "KDE")
-		STATE_CMD=kscreenlocker_state
-		KILL_CMD=kscreenlocker_kill
+	"kde4" | "KDE4")
+		STATE_CMD=kscreenlocker4_state
+		KILL_CMD=kscreenlocker4_kill
 		;;
-	*)
+	"kde" | "KDE" | "kde5" | "KDE5")
+		STATE_CMD=kscreenlocker5_state
+		KILL_CMD=kscreenlocker5_kill
+		;;
+	"x" | "X"|"x11"|"X11")
 		STATE_CMD=xscreensaver_state
 		KILL_CMD=xscreensaver_kill
 		;;
+	*)
+		echo "Unknown DESKTOP specified"
+		exit -1
 esac
 
 while true
 do	# Run only if screen is locked
 	$STATE_CMD
-	#PID=$(pgrep -u $ME $LOCKPRG | head -1)
+	#echo "LOCKED=$LOCKED"
 	if [ $LOCKED == 1 ]; then
 		# Ping the phone needs sudo permissions
 		# chmod u+s /usr/bin/l2ping
 		l2ping -t 5 -c 1 $BTHW >/dev/null 2>/dev/null
 		RC=$?
+		#echo "RC=$RC"
 		if [ $RC = 0 ]; then
 			# Phone present...
+			#echo "KILL"
 			$KILL_CMD
 			xset dpms force on
 		fi
